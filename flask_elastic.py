74f37ff97d9033bd67c75ea4aa266afdbdb75be1
Pablo Marti <pmargam@gmail.com>
2015-01-02 09:48:02 +0100

    some small fixes

--- flask_elastic.py
+++ flask_elastic.py
@@ -1,10 +1,10 @@
 from flask import current_app
-from elasticsearch import Elasticsearch
+import elasticsearch
 
 
 class Elastic(object):
     """
-    A thin wrapper around elasticsearch.Elasticsearch()
+    A thin wrapper around `elasticsearch.Elasticsearch`
     """
     def __init__(self, app=None, **kwargs):
         if app is not None:
@@ -13,15 +13,17 @@ class Elastic(object):
     def init_app(self, app, **kwargs):
         app.config.setdefault('ELASTICSEARCH_URL', 'http://localhost:9200/')
 
-        # using the app factory pattern _app_ctx_stack.top is None so what
-        # do we register on? app.extensions looks a little hackish (I don't
-        # know flask well enough to be sure), but that's how it's done in
-        # flask-pymongo so let's use it for now.
-        app.extensions['elastic'] = \
-            Elasticsearch(app.config['ELASTICSEARCH_URL'], **kwargs)
+        # for backwards compatibility
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+
+        app.extensions['elastic'] = elasticsearch.Elasticsearch(
+            app.config['ELASTICSEARCH_URL'],
+            **kwargs
+        )
 
     def __getattr__(self, item):
-        if not 'elastic' in current_app.extensions.keys():
+        if 'elastic' not in current_app.extensions:
             raise Exception(
                 'not initialised, did you forget to call init_app?')
         return getattr(current_app.extensions['elastic'], item)
